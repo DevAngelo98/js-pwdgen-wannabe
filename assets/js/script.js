ee58f36bf2cf6df3612ddb401c7c0974d6fbee0c
DevAngelo98 <ainglima07@gmail.com>
2019-10-17 01:46:51 +0200

    Random position

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,19 @@ var nome = document.getElementById("formNome").value;
 var cognome = document.getElementById("formCognome").value;
 var colore = document.getElementById("formColore").value;
 var password = nome+cognome+colore+"19";
-document.getElementById("formPass").innerHTML = password;
+
+// Creo l'array dalla stringa
+var arrayPassword = password.split("");
+
+// Genero un array random
+arrayPassword = arrayPassword.sort(function(){
+  return Math.random() - 0.5;
+});
+
+// Converto l'array in Stringa
+var finalPassword = arrayPassword.join('');
+
+document.getElementById("formPass").innerHTML = finalPassword;
+
 }
 
